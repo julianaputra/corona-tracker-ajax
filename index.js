67ca5d57c21fa7e354c90bf4692f824ee9fdc735
julianaputra <julianaputra10@gmail.com>
2020-05-12 21:04:49 +0800

    scrollable table, cards, countUp.js

--- index.js
+++ index.js
@@ -13,7 +13,23 @@ $(document).ready(function () {
           tbrow += showTbrow(d, i);
         }
       });
+
       $(".tbody").html(tbrow);
+    },
+    error: (e) => {
+      console.log(e.responseText);
+    },
+  });
+  $.ajax({
+    url: "https://indonesia-covid-19.mathdro.id/api",
+    success: (results) => {
+      let cards = showCards(results);
+
+      $(".card-container").html(cards);
+      $(".counter").counterUp({
+        delay: 10,
+        time: 1000,
+      });
     },
     error: (e) => {
       console.log(e.responseText);
@@ -29,4 +45,43 @@ function showTbrow(d, i) {
             <td>${d.kasusSemb}</td>
             <td>${d.kasusMeni}</td>
           </tr>`;
+}
+
+function showCards(r) {
+  return `  <div class="col-xs-12 col-sm-4">
+              <div class="card text-center">
+                <div class="card-header bg-warning">
+                  <h6 class="card-title m-0">
+                    POSITIF
+                  </h6>
+                </div>
+                <div class="card-body">
+                  <h2 class="counter">${r.jumlahKasus}</h2>
+                </div>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-4">
+              <div class="card text-center">
+                <div class="card-header bg-success">
+                  <h6 class="card-title m-0">
+                    SEMBUH
+                  </h6>
+                </div>
+                <div class="card-body">
+                  <h2 class="counter">${r.sembuh}</h2>
+                </div>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-4">
+              <div class="card text-center">
+                <div class="card-header bg-danger">
+                  <h6 class="card-title m-0">
+                    MENINGGAL
+                  </h6>
+                </div>
+                <div class="card-body">
+                  <h2 class="counter">${r.meninggal}</h2>
+                </div>
+              </div>
+            </div>`;
 }
