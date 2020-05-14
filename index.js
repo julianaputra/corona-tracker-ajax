cb07b9c352286ec8c0e9036f12684dc3b5f90103
julianaputra <julianaputra10@gmail.com>
2020-05-14 14:52:28 +0800

    add fontawesome icon, media screen, numeral.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
       $(".card-container").html(cards);
       $(".counter").counterUp({
         delay: 10,
-        time: 1000,
+        time: 2000,
       });
     },
     error: (e) => {
@@ -41,47 +41,62 @@ function showTbrow(d, i) {
   return ` <tr>
             <th scope="row">${i}</th>
             <td>${d.provinsi}</td>
-            <td>${d.kasusPosi}</td>
-            <td>${d.kasusSemb}</td>
-            <td>${d.kasusMeni}</td>
+            <td>${numeral(d.kasusPosi).format(0, 0)}</td>
+            <td>${numeral(d.kasusSemb).format(0, 0)}</td>
+            <td>${numeral(d.kasusMeni).format(0, 0)}</td>
           </tr>`;
 }
 
 function showCards(r) {
-  return `  <div class="col-xs-12 col-sm-4">
-              <div class="card text-center mb-3">
-                <div class="card-header bg-warning">
-                  <h6 class="card-title m-0">
-                    POSITIF
-                  </h6>
-                </div>
-                <div class="card-body">
-                  <h2 class="counter">${r.jumlahKasus}</h2>
-                </div>
-              </div>
-            </div>
-            <div class="col-xs-12 col-sm-4">
-              <div class="card text-center mb-3">
-                <div class="card-header bg-success">
-                  <h6 class="card-title m-0">
-                    SEMBUH
-                  </h6>
-                </div>
-                <div class="card-body">
-                  <h2 class="counter">${r.sembuh}</h2>
+  return ` <div class="col-md-4">
+            <div class="card text-center mb-3 MyShadow" style="border-left: 8px solid #FF9960;">
+              <div class="card-body">
+                <div class="row d-flex justify-content-start align-items-center">
+                  <div class="col-4">
+                   
+                      <i class="icon fas fa-virus p-3 rounded-circle" style="background-color: #FF996030; font-size: 3em; color: #FF9960;"></i>
+             
+                  </div>
+                  <div class="col-8 text-left">
+                    <h3 class="card-title m-0 p-0 font-weight-bold text-secondary" style="font-size: 2rem";>Positif</h3>
+                    <h3 class="counter m-0 p-0 font-weight-bold" style="color: #FF9960;">${numeral(r.jumlahKasus).format(0, 0)}</h3>
+                  </div>
                 </div>
               </div>
             </div>
-            <div class="col-xs-12 col-sm-4">
-              <div class="card text-center mb-3">
-                <div class="card-header bg-danger">
-                  <h6 class="card-title m-0">
-                    MENINGGAL
-                  </h6>
-                </div>
-                <div class="card-body">
-                  <h2 class="counter">${r.meninggal}</h2>
+          </div>
+
+          <div class="col-md-4">
+            <div class="card text-center mb-3 MyShadow" style="border-left: 8px solid #6BD37C;">
+              <div class="card-body">
+                <div class="row d-flex justify-content-start align-items-center">
+                  <div class="col-4">
+                  
+                      <i class="icon fas fa-heartbeat p-3 rounded-circle" style="background-color: #6BD37C30; font-size: 3em; color: #6BD37C;"></i>
+           
+                  </div>
+                  <div class="col-8 text-left">
+                    <h3 class="card-title m-0 p-0 font-weight-bold text-secondary">Sembuh</h3>
+                    <h3 class="counter m-0 p-0 font-weight-bold" style="color: #6BD37C;">${numeral(r.sembuh).format(0, 0)}</h3>
+                  </div>
                 </div>
               </div>
-            </div>`;
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="card text-center mb-3 MyShadow" style="border-left: 8px solid #FF6060;">
+              <div class="card-body">
+                <div class="row d-flex justify-content-start align-items-center">
+                  <div class="col-4">                
+                      <i class="icon fas fa-skull p-3 rounded-circle" style="background-color: #FF606030; font-size: 3em; color: #FF6060;"></i>
+                  </div>
+                  <div class="col-8 text-left">
+                    <h3 class="card-title m-0 p-0 font-weight-bold text-secondary">Meninggal</h3>
+                    <h3 class="counter m-0 p-0 font-weight-bold" style="color: #FF6060;">${numeral(r.meninggal).format(0, 0)}</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>`;
 }
